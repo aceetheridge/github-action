9d4a4bdbfc199c5012fccb5bb3cbc52e5dd285ee
aceetheridge <aceetheridge@gmail.com>
2021-04-21 22:05:09 -0400

    jfkdls

--- microservice/server.go
+++ microservice/server.go
@@ -7,9 +7,6 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!\n")
 }
-func blah(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf( w,"YO dawg")
-}
 func NewServer(host string, port string) *http.Server {
 	addr := fmt.Sprintf("%s:%s", host, port)
 
